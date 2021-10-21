90fbd7745504be174ace315dead3095c0286f6c4
tiberiu_ionescu <tiberiu_ionescu@users.noreply.github.com>
2021-10-21 13:48:45 +0300

    First edition

---
+++ develop/AsyncBreakfastC/AsyncBreakfastC/Model/Egg.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncBreakfastC.Model
+{
+    class Egg
+    {
+        public static int eggsBoiled = 0;
+
+        public Egg()
+        {
+            eggsBoiled++;
+        }
+    }
+}
---
+++ develop/AsyncBreakfastC/AsyncBreakfastC/Model/Toast.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncBreakfastC.Model
+{
+    class Toast
+    {
+        public static int toastsMade = 0;
+
+        public Toast()
+        {
+            toastsMade++;
+        }
+    }
+}
